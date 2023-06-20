bf3694d8accc59942695cb9c0393fe31b90a4347
Eva Sorensen <eva.sorensen@halter.co.nz>
2023-06-21 11:32:56 +1200

    add farm and building svgs

---
+++ dashboard/src/data.ts
@@ -0,0 +1,92 @@
+export type Farm = {
+  farmName: string;
+  buildings: Building[];
+};
+
+export type Building = {
+  buildingName: string;
+  plots: Plot[];
+};
+
+export type Plot = {
+  plotName: string;
+  plants: Plant[];
+};
+
+export type Plant = {
+  plantName: string;
+};
+
+export default [
+  {
+    farmName: 'Farm 1',
+    buildings: [
+      {
+        buildingName: 'Building 1',
+        plots: [
+          {
+            plotName: 'Plot A1',
+            plants: [
+              {
+                plantName: '003',
+              },
+            ],
+          },
+          {
+            plotName: 'Plot A2',
+            plants: [],
+          },
+          {
+            plotName: 'Plot A3',
+            plants: [],
+          },
+          {
+            plotName: 'Plot B1',
+            plants: [],
+          },
+          {
+            plotName: 'Plot B2',
+            plants: [],
+          },
+          {
+            plotName: 'Plot B3',
+            plants: [],
+          },
+          {
+            plotName: 'Plot C1',
+            plants: [],
+          },
+          {
+            plotName: 'Plot C2',
+            plants: [],
+          },
+          {
+            plotName: 'Plot C3',
+            plants: [],
+          },
+        ],
+      },
+      {
+        buildingName: 'Building 2',
+        plots: [],
+      },
+      {
+        buildingName: 'Building 3',
+        plots: [],
+      },
+      {
+        buildingName: 'Building 4',
+        plots: [],
+      },
+    ],
+  },
+  {
+    farmName: 'Farm 2',
+    buildings: [
+      {
+        buildingName: 'Main Greenhouse',
+        plots: [],
+      },
+    ],
+  },
+];
